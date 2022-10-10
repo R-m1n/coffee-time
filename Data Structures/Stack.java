e7b7836a3c6bd0a213ef1ed671026dbf9f9a1aef
Armin Abbasi <apmnh.armin@gmail.com>
2022-10-10 23:13:24 +0200

    Refactor Stack.java

--- Data Structures/Stack.java
+++ Data Structures/Stack.java
@@ -1,6 +1,38 @@
 import java.util.Arrays;
 import java.util.EmptyStackException;
 
+/* 
+Stack implementation.
+
+Attributes
+----------
+private {
+stack: int[]
+    Underlying array.
+count: int
+    Current pointer in the stack.
+max: int
+    Maximum value in the stack.
+}
+
+Methods
+-------
+public {
+push(int item): void
+    Add an item to the top of the stack.
+peek(): int
+    Return the last item in the stack.
+pop(): int
+    Return and remove the last item in the stack.
+max(): int
+    Return the maximum value int the stack.
+min(): int
+    Return the minimum value int the stack.
+isEmpty(): boolean
+    Check if the stack is empty.
+}
+*/
+
 public class Stack {
     private int[] stack;
     private int count = 0;
@@ -10,21 +42,9 @@ public class Stack {
         this.stack = new int[size];
     }
 
-    public int pop() {
-        if (this.isEmpty())
-            throw new EmptyStackException();
-
-        return this.stack[--this.count];
-    }
-
-    public int peek() {
-        if (this.isEmpty())
-            throw new EmptyStackException();
-
-        return this.stack[this.count - 1];
-    }
-
     public void push(int item) {
+        // Add an item to the top of the stack.
+
         if (this.count == stack.length)
             throw new StackOverflowError();
 
@@ -34,11 +54,33 @@ public class Stack {
         this.stack[this.count++] = item;
     }
 
+    public int peek() {
+        // Return the last item in the stack.
+
+        if (this.isEmpty())
+            throw new EmptyStackException();
+
+        return this.stack[this.count - 1];
+    }
+
+    public int pop() {
+        // Return and remove the last item in the stack.
+
+        if (this.isEmpty())
+            throw new EmptyStackException();
+
+        return this.stack[--this.count];
+    }
+
     public int max() {
+        // Return the maximum value int the stack.
+
         return this.max;
     }
 
     public int min() {
+        // Return the minimum value int the stack.
+
         int count_holder = this.count;
         int min = this.max();
 
@@ -57,6 +99,8 @@ public class Stack {
     }
 
     public boolean isEmpty() {
+        // Check if the stack is empty.
+
         return this.count == 0;
     }
 
