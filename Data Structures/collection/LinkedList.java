92a9113b1eb4c9514595461f6b41f778e59cb9ae
Armin Abbasi <apmnh.armin@gmail.com>
2022-10-15 01:05:40 +0200

    javadoc LinkedList.java

--- Data Structures/collection/LinkedList.java
+++ Data Structures/collection/LinkedList.java
@@ -2,114 +2,46 @@ package collection;
 
 import java.util.Arrays;
 import java.util.NoSuchElementException;
-/* 
-Linked List implementation.
 
-Classes
--------
-private {
-Node()
-    Attributes
-    ----------
-    private {
-        value: int
-            Assigned value of the Node.
-        next: Next
-            Reference to the next Node in the list.
-    }
-
-    Methods
-    -------
-    public {
-        getNext(): Node
-            Get the reference of the next Node.
-        getValue(): int
-            Get the assigned value of the Node.
-        setNext(): void
-            Set the reference of the next Node.
-        setValue(): void
-            Assign a value to the Node. 
-    }
-}
-
-Attributes
-----------
-public {
-size: int
-    Size of the list.
-}
-
-private {
-first: Node
-    Head of the linked list.
-last: Node
-    Tail of the linked list.
-}
-
-Methods
--------
-public {
-addFirst(int value): void
-    Add an item to the beginning of the list.
-addLast(int value): void
-    Add an item to the end of the list.
-deleteFirst(): void
-    Delete the first item in the list.
-deleteLast(): void
-    Delete the last item in the list.
-index(int value): int
-    Return index of the given value.
-get(int index): int
-    Return the item in the given index.
-contains(int value): boolean
-    Check if the array contains the given value.
-toArray(): int[]
-    Convert list to an array.
-reverse(): void
-    Reverse the array inplace.
-middle(): int[]
-    Return the middle of the list.
-}
-
-private {
-secondToLastNode(Node first_node, Node last_node): void
-    Return the second to last node.
-isEmpty(): boolean
-    Check if the list is empty.
-isNull(): boolean
-    Check if the Node is null.
-check(): void
-    Throw exception if the list is empty, set the Head 
-    and the Tail reference to null and the size to zero.
-}
-*/
-
+/**
+ * A Java implementation of Linked List data structure.
+ * 
+ * @author R-m1n
+ */
 public class LinkedList {
     private class Node {
         private int value;
         private Node next;
 
+        /**
+         * @return the reference of the next Node.
+         */
         public Node getNext() {
-            // Get the reference of the next Node.
-
             return next;
         }
 
+        /**
+         * @return the value of the Node.
+         */
         public int getValue() {
-            // Get the assigned value of the Node.
-
             return value;
         }
 
+        /**
+         * Set the reference of the next Node.
+         * 
+         * @param next
+         */
         public void setNext(Node next) {
-            // Set the reference of the next Node.
-
             this.next = next;
         }
 
+        /**
+         * Set the value of the Node.
+         * 
+         * @param value
+         */
         public void setValue(int value) {
-            // Assign a value to the Node.
-
             this.value = value;
         }
     }
@@ -118,9 +50,12 @@ public class LinkedList {
     private Node first = new Node();
     private Node last = new Node();
 
+    /**
+     * Add an item to the beginning of the list.
+     * 
+     * @param value
+     */
     public void addFirst(int value) {
-        // Add an item to the beginning of the list.
-
         Node node = new Node();
         node.setNext(this.first);
         node.setValue(value);
@@ -134,9 +69,12 @@ public class LinkedList {
         this.size++;
     }
 
+    /**
+     * Add an item to the end of the list.
+     * 
+     * @param value
+     */
     public void addLast(int value) {
-        // Add an item to the end of the list.
-
         Node node = new Node();
         node.setValue(value);
         this.last.setNext(node);
@@ -150,9 +88,10 @@ public class LinkedList {
         this.size++;
     }
 
+    /**
+     * Delete the first item in the list.
+     */
     public void deleteFirst() {
-        // Delete the first item in the list.
-
         this.check();
         Node second = this.first.getNext();
         this.first = null;
@@ -160,9 +99,10 @@ public class LinkedList {
         this.size--;
     }
 
+    /**
+     * Delete the last item in the list.
+     */
     public void deleteLast() {
-        // Delete the last item in the list.
-
         this.check();
         Node node = secondToLastNode(this.first, this.last);
         node.setNext(null);
@@ -171,9 +111,11 @@ public class LinkedList {
         this.size--;
     }
 
-    public int index(int value) {
-        // Return index of the given value.
-
+    /**
+     * @param value
+     * @return index of the given value.
+     */
+    public int indexOf(int value) {
         Node node = first;
         int index = 0;
 
@@ -188,14 +130,18 @@ public class LinkedList {
         return -1;
     }
 
+    /**
+     * @param index
+     * @return the item in the given index.
+     */
     public int get(int index) {
-        // Return the item in the given index.
+        Node node;
 
         if (index < 0) {
             index = size + index;
         }
 
-        Node node = this.first;
+        node = this.first;
 
         for (int i = 0; i < index; i++) {
             node = node.getNext();
@@ -204,15 +150,25 @@ public class LinkedList {
         return node.getValue();
     }
 
+    /**
+     * @param value
+     * @return true if the list contains the given value, else false.
+     */
     public boolean contains(int value) {
-        // Check if the array contains the given value.
-
-        return index(value) != -1;
+        return indexOf(value) != -1;
     }
 
-    public int[] toArray() {
-        // Convert list to an array.
+    /**
+     * @return true if the list is empty, else false.
+     */
+    public boolean isEmpty() {
+        return this.size == 0;
+    }
 
+    /**
+     * @return array of the items in the list.
+     */
+    public int[] toArray() {
         int[] array = new int[this.size];
         Node node = this.first;
 
@@ -224,9 +180,10 @@ public class LinkedList {
         return array;
     }
 
+    /**
+     * Reverse the list inplace.
+     */
     public void reverse() {
-        // Reverse the list inplace.
-
         Node node = new Node();
         Node second_to_last;
         int counter = 0;
@@ -244,9 +201,10 @@ public class LinkedList {
         this.first = node;
     }
 
+    /**
+     * @return an array of the items in the middle of the list.
+     */
     public int[] middle() {
-        // Return the middle of the list.
-
         if (this.size < 3) {
             return this.toArray();
         }
@@ -281,9 +239,10 @@ public class LinkedList {
         }
     }
 
+    /**
+     * @return true if the list has a loop, else false.
+     */
     public boolean hasLoop() {
-        // Check whether the list has a loop.
-
         Node n1 = this.first;
         Node n2 = this.first.getNext().getNext();
 
@@ -299,9 +258,12 @@ public class LinkedList {
         return false;
     }
 
+    /**
+     * @param first_node
+     * @param last_node
+     * @return the second to last node.
+     */
     private Node secondToLastNode(Node first_node, Node last_node) {
-        // Return the second to last node.
-
         Node node = first_node;
 
         while (!isNull(node)) {
@@ -314,31 +276,33 @@ public class LinkedList {
         return null;
     }
 
-    private boolean isEmpty() {
-        // Check if the list is empty.
-
-        return this.size == 0;
-    }
-
+    /**
+     * @param node
+     * @return true if the Node is null, else false.
+     */
     private boolean isNull(Node node) {
-        // Check if the Node is null.
-
         return node == null;
     }
 
+    /**
+     * Set the Head and the Tail reference to null and the size to zero,
+     * if the list has one item.
+     */
     private void check() {
-        /*
-         * Throw exception if the list is empty, set the Head
-         * and the Tail reference to null and the size to zero.
-         */
-
-        if (isEmpty()) {
-            throw new NoSuchElementException();
-        }
+        checkEmpty();
 
         if (this.size == 1) {
             this.first = this.last = null;
             this.size = 0;
+        }
+    }
+
+    /**
+     * @throws NoSuchElementException if the list is empty.
+     */
+    private void checkEmpty() {
+        if (isEmpty()) {
+            throw new NoSuchElementException();
         }
     }
 
