4143ed0128652e44c3ead4d228e787af55107ae9
Armin Abbasi <apmnh.armin@gmail.com>
2022-10-15 01:23:38 +0200

    javadoc Quack.java

--- Data Structures/collection/Quack.java
+++ Data Structures/collection/Quack.java
@@ -3,83 +3,59 @@ package collection;
 import java.util.ArrayDeque;
 import java.util.Queue;
 
-/* 
-Stack implementation using Queues.
-
-Attributes
-----------
-private {
-s1: Stack<Integer>
-    A stack for enqueuing operation.
-s2: Stack<Integer>
-    A stack for enqueuing operation.
-}
-
-Methods
--------
-public {
-push(int item): void
-    Add an item to the top of the stack.
-peek(): int
-    Return the item at the top of the stack.
-pop(): int
-    Return and remove the item at the top of the stack.
-isEmpty(): booelean
-    Check if the stack is empty.
-size(): int
-    Return the size of the stack.
-}
-
-private {
-removeTop(): int
-    Return and remove the item at the top of the first queue.
-getTop(): int
-    Return the item at the top of the first queue.
-checkEmpty(): void
-    Throw an exception if the stack is empty.
-}
-*/
-
+/**
+ * A Java implementation of Stack data structure using Queues.
+ * 
+ * @author R-m1n
+ */
 public class Quack implements Stack {
     private Queue<Integer> q1 = new ArrayDeque<>();
     private Queue<Integer> q2 = new ArrayDeque<>();
     private int count = 0;
 
+    /**
+     * Add an item to the top of the stack.
+     * 
+     * @param item
+     */
     public void push(int item) {
-        // Add an item to the top of the stack.
-
         q1.add(item);
         count++;
     }
 
+    /**
+     * @return the item at the top of the stack.
+     */
     public int peek() {
-        // Return the item at the top of the stack.
-
         return getTop();
     }
 
+    /**
+     * @return and remove the item at the top of the stack.
+     */
     public int pop() {
-        // Return and remove the item at the top of the stack.
-
         checkEmpty();
         return removeTop();
     }
 
+    /**
+     * @return true if the stack is empty, else false.
+     */
     public boolean isEmpty() {
-        // Check if the stack is empty.
-
         return q1.isEmpty() && q2.isEmpty();
     }
 
+    /**
+     * @return the size of the stack.
+     */
     public int size() {
-        // Return the size of the stack.
-
         return count;
     }
 
+    /**
+     * @return and remove the item at the top of the first queue.
+     */
     private int removeTop() {
-        // Return and remove the item at the top of the first queue.
-
         int top;
 
         for (int i = 0; i < count - 1; i++) {
@@ -97,9 +73,10 @@ public class Quack implements Stack {
         return top;
     }
 
+    /**
+     * @return the item at the top of the first queue.
+     */
     private int getTop() {
-        // Return the item at the top of the first queue.
-
         int top;
 
         for (int i = 0; i < count - 1; i++) {
@@ -117,9 +94,10 @@ public class Quack implements Stack {
         return top;
     }
 
+    /**
+     * @throws IllegalStateException
+     */
     private void checkEmpty() {
-        // Throw an exception if the stack is empty.
-
         if (this.isEmpty())
             throw new IllegalStateException();
     }
