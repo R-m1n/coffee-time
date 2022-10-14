e31bd0dc76320e5877fa79253821577b0b8c2363
Armin Abbasi <apmnh.armin@gmail.com>
2022-10-15 01:34:42 +0200

    javadoc Sueue.java

--- Data Structures/collection/Sueue.java
+++ Data Structures/collection/Sueue.java
@@ -2,81 +2,61 @@ package collection;
 
 import java.util.Stack;
 
-/* 
-Queue implementation using Stacks.
-
-Attributes
-----------
-private {
-s1: Stack<Integer>
-    A stack for enqueuing operation.
-s2: Stack<Integer>
-    A stack for enqueuing operation.
-}
-
-Methods
--------
-public {
-enqueue(int item): void
-    Add an item to the back of the queue.
-dequeue(): int
-    Return and remove an item from the front of the queue.
-peek(): int
-    Return an item from the front of the queue.
-isEmpty(): booelean
-    Check if the queue is empty.
-}
-
-private {
-changeStack(): void
-    Pushing items to the second stack.
-checkEmpty(): void
-    Throw an exception if the queue is empty.
-}
-*/
-
+/**
+ * A Java implementation of Queue data structure using Stacks.
+ * 
+ * @author R-m1n
+ */
 public class Sueue implements Queue {
     private Stack<Integer> s1 = new Stack<>();;
     private Stack<Integer> s2 = new Stack<>();
 
+    /**
+     * Add an item to the back of the queue.
+     * 
+     * @param item
+     */
     public void enqueue(int item) {
-        // Add an item to the back of the queue.
-
         s1.push(item);
     }
 
+    /**
+     * @return and remove an item from the front of the queue.
+     */
     public int dequeue() {
-        // Return and remove an item from the front of the queue.
-
         checkEmpty();
         changeStack();
         return s2.pop();
     }
 
+    /**
+     * @return an item from the front of the queue.
+     */
     public int peek() {
-        // Return an item from the front of the queue.
-
         changeStack();
         return s2.peek();
     }
 
+    /**
+     * @return true if the queue is empty, else false.
+     */
     public boolean isEmpty() {
-        // Check if the queue is empty.
-
         return s1.empty() && s2.empty();
     }
 
+    /**
+     * Pushing items to the second stack.
+     */
     private void changeStack() {
-        // Pushing items to the second stack.
-
         if (s2.empty())
             while (!s1.empty())
                 s2.push(s1.pop());
     }
 
+    /**
+     * @throws IllegalStateException if the queue is empty.
+     */
     private void checkEmpty() {
-        // Throw an exception if the queue is empty.
-
         if (this.isEmpty())
             throw new IllegalStateException();
     }
