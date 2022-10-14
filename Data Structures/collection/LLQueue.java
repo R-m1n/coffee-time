d908ebb34858203c4a99c338462b036967f4f4aa
Armin Abbasi <apmnh.armin@gmail.com>
2022-10-15 01:11:57 +0200

    javadoc LLQueue.java

--- Data Structures/collection/LLQueue.java
+++ Data Structures/collection/LLQueue.java
@@ -3,29 +3,54 @@ package collection;
 import java.util.Arrays;
 import java.util.LinkedList;
 
+/**
+ * A Java implementation of Queue data structure using Linked List.
+ * 
+ * @author R-m1n
+ */
 public class LLQueue implements Queue {
     LinkedList<Integer> ll = new LinkedList<>();
 
+    /**
+     * Add an item to the back of the queue.
+     * 
+     * @param item
+     */
     public void enqueue(int item) {
         ll.addLast(item);
     }
 
+    /**
+     * @return and remove an item from the beginning of the queue.
+     */
     public int dequeue() {
         return ll.removeFirst();
     }
 
+    /**
+     * @return an item from the beginning of the queue.
+     */
     public int peek() {
         return ll.getFirst();
     }
 
+    /**
+     * @return true if the queue is empty, else false.
+     */
     public boolean isEmpty() {
         return ll.isEmpty();
     }
 
+    /**
+     * @return the size of the queue.
+     */
     public int size() {
         return ll.size();
     }
 
+    /**
+     * @return an array of the items in the queue.
+     */
     public Object[] toArray() {
         return ll.toArray();
     }
