69895aa8ecdf6b31dbdbce87d83bd30494b303c8
Armin Abbasi <apmnh.armin@gmail.com>
2022-10-18 21:26:58 +0200

    Refactor BinaryTree.java

--- src/main/java/com/datastructures/nonLinear/collection/BinaryTree.java
+++ src/main/java/com/datastructures/nonLinear/collection/BinaryTree.java
@@ -106,16 +106,7 @@ public class BinaryTree {
 
     public int height(Node node) {
         if (node == null)
-            return 0;
-
-        else if (node.getLeft() == null && node.getRight() == null)
-            return 0;
-
-        else if (node.getLeft() == null)
-            node = node.getRight();
-
-        else if (node.getRight() == null)
-            node = node.getLeft();
+            return -1;
 
         return 1 + Math.max(height(node.getLeft()), height(node.getRight()));
     }
