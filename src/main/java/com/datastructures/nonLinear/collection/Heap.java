e09a56de7fa497141c50d8a270461a5d651e9e47
Armin Abbasi <apmnh.armin@gmail.com>
2022-10-27 21:00:04 +0200

    Add feature to Heap.java

--- src/main/java/com/datastructures/nonLinear/collection/Heap.java
+++ src/main/java/com/datastructures/nonLinear/collection/Heap.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
 public class Heap {
 
     private Integer[] nodes = new Integer[10];
+    private int last;
 
     public void insert(int value) {
         for (int i = 0; i < nodes.length; i++) {
@@ -13,48 +14,80 @@ public class Heap {
 
             if (nodes[i] == null) {
                 nodes[i] = value;
+                last = i;
                 return;
             }
 
             else if (nodes[left] == null) {
                 nodes[left] = value;
-                bubbleUpLeft(left);
+                bubbleUp(left);
+                last = left;
                 return;
             }
 
             else if (nodes[right] == null) {
                 nodes[right] = value;
-                bubbleUpRight(right);
+                bubbleUp(right);
+                last = right;
                 return;
             }
         }
     }
 
-    public void swap(int i, int j) {
+    public void remove() {
+        nodes[0] = nodes[last];
+        nodes[last] = null;
+        bubbleDown(0);
+    }
+
+    private void swap(int i, int j) {
         int node = nodes[i];
         nodes[i] = nodes[j];
         nodes[j] = node;
     }
 
-    private void bubbleUpLeft(int i) {
+    private void bubbleUp(int i) {
         if (i == 0)
             return;
 
-        int parent = (i - 1) / 2;
+        int parent = i % 2 == 0 ? (i - 2) / 2 : (i - 1) / 2;
         if (nodes[i] > nodes[parent]) {
             swap(i, parent);
-            bubbleUpLeft(parent);
+            bubbleUp(parent);
         }
     }
 
-    private void bubbleUpRight(int i) {
-        if (i == 0)
+    private void bubbleDown(int i) {
+        int left = 2 * i + 1;
+        int right = 2 * i + 2;
+
+        if (i == last)
             return;
 
-        int parent = (i - 2) / 2;
-        if (nodes[i] > nodes[parent]) {
-            swap(i, parent);
-            bubbleUpRight(parent);
+        if (!(left >= nodes.length || right >= nodes.length)) {
+            if (nodes[right] == null || nodes[left] == null)
+                return;
+
+            if (nodes[right] > nodes[left]) {
+                swap(i, right);
+                bubbleDown(right);
+            }
+
+            else if (nodes[right] < nodes[left]) {
+                swap(i, left);
+                bubbleDown(left);
+            }
+
+        }
+
+        else if (right >= nodes.length) {
+            swap(i, left);
+            bubbleDown(left);
+        }
+
+        else if (left >= nodes.length) {
+            swap(i, right);
+            bubbleDown(right);
         }
     }
 
