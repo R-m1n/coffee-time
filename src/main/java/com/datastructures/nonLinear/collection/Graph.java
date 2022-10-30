f29e94f4f9ef740025ba173e0b830d618d61db84
Armin Abbasi <apmnh.armin@gmail.com>
2022-10-30 21:39:47 +0100

    javadoc Trie.java

--- src/main/java/com/datastructures/nonLinear/collection/Graph.java
+++ src/main/java/com/datastructures/nonLinear/collection/Graph.java
@@ -5,18 +5,6 @@ import java.util.HashMap;
 import java.util.LinkedList;
 
 public class Graph {
-    private class Node {
-        private String label;
-
-        public Node(String label) {
-            this.label = label;
-        }
-
-        public String getLabel() {
-            return label;
-        }
-    }
-
     private ArrayList<LinkedList<String>> list = new ArrayList<>();
     private HashMap<String, Integer> map = new HashMap<>();
 
--- src/main/java/com/datastructures/nonLinear/collection/Trie.java
+++ src/main/java/com/datastructures/nonLinear/collection/Trie.java
@@ -3,6 +3,11 @@ package src.main.java.com.datastructures.nonLinear.collection;
 import java.util.ArrayList;
 import java.util.HashMap;
 
+/**
+ * A Java implementation of Trie data structure.
+ * 
+ * @author R-m1n
+ */
 public class Trie {
     private class Node {
         private char character;
@@ -15,40 +20,73 @@ public class Trie {
             this.end = false;
         }
 
-        public void setEnd() {
-            this.end = true;
-        }
-
-        public void removeEnd() {
-            this.end = false;
-        }
-
-        public boolean isEnd() {
-            return end;
-        }
-
+        /**
+         * Add a character as a child of this Node.
+         * 
+         * @param character
+         */
         public void addChild(char character) {
             children.put(character, new Node(character));
         }
 
-        public boolean hasChild(char character) {
-            return children.containsKey(character);
-        }
-
+        /**
+         * @param character
+         * @return child of this Node with input as its character.
+         */
         public Node getChild(char character) {
             return children.getOrDefault(character, null);
         }
 
+        /**
+         * Remove child of this Node with input as its character.
+         * 
+         * @param character
+         */
         public void removeChild(char character) {
             children.remove(character);
         }
 
+        /**
+         * @param character
+         * @return true if this Node has input as its child.
+         */
+        public boolean hasChild(char character) {
+            return children.containsKey(character);
+        }
+
+        /**
+         * @return array of children of this Node.
+         */
         public Node[] getChildren() {
             return children.values().toArray(new Node[0]);
         }
 
+        /**
+         * @return true if this Node has children, else false.
+         */
         public boolean hasChildren() {
             return !children.isEmpty();
+        }
+
+        /**
+         * Set this Node as the end of a word.
+         */
+        public void setEnd() {
+            this.end = true;
+        }
+
+        /**
+         * Remove this Node as the end of a word.
+         */
+        public void removeEnd() {
+            this.end = false;
+        }
+
+        /**
+         * @return true if this Node is the end of a word, else false.
+         */
+        public boolean isEnd() {
+            return end;
         }
 
         @Override
@@ -59,9 +97,14 @@ public class Trie {
 
     private Node root = new Node(' ');
 
+    /**
+     * Insert input in Trie.
+     * 
+     * @param word
+     */
     public void insert(String word) {
-
         Node current = root;
+
         for (char character : word.toCharArray()) {
             if (!current.hasChild(character))
                 current.addChild(character);
@@ -72,6 +115,10 @@ public class Trie {
         current.setEnd();
     }
 
+    /**
+     * @param word
+     * @return true if Trie contains input, else false.
+     */
     public boolean contains(String word) {
         if (word == null)
             return false;
@@ -79,24 +126,40 @@ public class Trie {
         return contains(root, word, 0);
     }
 
-    public String[] complete(String prefix) {
+    /**
+     * @param prefix
+     * @return array of suggested words with given prefix.
+     */
+    public String[] autoComplete(String prefix) {
         if (prefix == null)
             return null;
 
         ArrayList<String> list = new ArrayList<>();
 
-        complete(lastNodeOf(prefix), prefix.strip(), list);
+        autoComplete(lastNodeOf(prefix), prefix.strip(), list);
         return list.toArray(new String[0]);
     }
 
+    /**
+     * Remove input from Trie.
+     * 
+     * @param word
+     */
     public void remove(String word) {
         remove(root, word, 0);
     }
 
+    /**
+     * @return number of words in the Trie.
+     */
     public int count() {
-        return complete("").length;
+        return autoComplete("").length;
     }
 
+    /**
+     * @param array
+     * @return longest common prefix of words in input array.
+     */
     public String lcp(String[] array) {
         if (array == null || array.length == 0)
             return "";
@@ -136,7 +199,7 @@ public class Trie {
             root.removeChild(character);
     }
 
-    private void complete(Node node, String prefix, ArrayList<String> list) {
+    private void autoComplete(Node node, String prefix, ArrayList<String> list) {
         if (node == null)
             return;
 
@@ -144,7 +207,7 @@ public class Trie {
             list.add(prefix);
 
         for (Node child : node.getChildren()) {
-            complete(child, prefix + child, list);
+            autoComplete(child, prefix + child, list);
         }
     }
 
