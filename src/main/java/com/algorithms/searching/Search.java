34739ebfa35ffbf17248e424df3646bd73ab121a
Armin Abbasi <apmnh.armin@gmail.com>
2022-11-04 23:04:41 +0100

    Add feature to Search.java

--- src/main/java/com/algorithms/searching/Search.java
+++ src/main/java/com/algorithms/searching/Search.java
@@ -9,4 +9,48 @@ public class Search {
         return -1;
     }
 
+    public static int binary(int[] array, int item) {
+        int start = 0;
+        int end = array.length - 1;
+
+        int middle;
+        while (start <= end) {
+            middle = (start + end) / 2;
+
+            if (item == array[middle])
+                return middle;
+
+            if (item < array[middle])
+                end = middle - 1;
+
+            else
+                start = middle + 1;
+        }
+
+        return -1;
+    }
+
+    public static int binary(int[] array, int item, boolean recursive) {
+        if (recursive)
+            return binary(array, item, 0, array.length - 1);
+
+        return binary(array, item);
+    }
+
+    private static int binary(int[] array, int item, int start, int end) {
+        if (start > end)
+            return -1;
+
+        int middle = (start + end) / 2;
+
+        if (item == array[middle])
+            return middle;
+
+        else if (item < array[middle])
+            return binary(array, item, start, middle - 1);
+
+        else
+            return binary(array, item, middle + 1, end);
+    }
+
 }
--- src/test/java/com/algorithms/TestSearch.java
+++ src/test/java/com/algorithms/TestSearch.java
@@ -8,10 +8,27 @@ import src.main.java.com.algorithms.searching.Search;
 
 public class TestSearch {
     int[] array = { 121, 432, 564, 23, 1, 45, 788 };
+    int[] sortedArray = { 3, 5, 6, 9, 11, 18, 20, 21, 24, 30 };
 
     @Test
     public void testLinearSearch() {
         assertEquals(Search.linear(array, 1), 4);
         assertEquals(Search.linear(array, 0), -1);
+    }
+
+    @Test
+    public void testBinarySearch() {
+        assertEquals(Search.binary(sortedArray, 18), 5);
+        assertEquals(Search.binary(sortedArray, 3), 0);
+        assertEquals(Search.binary(sortedArray, 30), 9);
+        assertEquals(Search.binary(sortedArray, 21), 7);
+        assertEquals(Search.binary(sortedArray, 0), -1);
+        assertEquals(Search.binary(sortedArray, 100), -1);
+        assertEquals(Search.binary(sortedArray, 18, true), 5);
+        assertEquals(Search.binary(sortedArray, 3, true), 0);
+        assertEquals(Search.binary(sortedArray, 30, true), 9);
+        assertEquals(Search.binary(sortedArray, 21, true), 7);
+        assertEquals(Search.binary(sortedArray, 0, true), -1);
+        assertEquals(Search.binary(sortedArray, 100, true), -1);
     }
 }
